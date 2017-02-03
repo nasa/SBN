a52fc9ae3cca358968fbbe2445213194399f427d
Chris Knight <Christopher.D.Knight@nasa.gov>
2017-02-02 19:47:19 -0800

    Added draft DTN module.

---
+++ modules/dtn/fsw/src/sbn_dtn_if.h
@@ -0,0 +1,41 @@
+#ifndef _sbn_dtn_if_h_
+#define _sbn_dtn_if_h_
+
+#include "sbn_constants.h"
+#include "sbn_interfaces.h"
+#include "cfe.h"
+
+int SBN_DTN_LoadNet(const char **Row, int FieldCount, SBN_NetInterface_t *Net);
+
+int SBN_DTN_LoadPeer(const char **Row, int FieldCount,
+    SBN_PeerInterface_t *Peer);
+
+int SBN_DTN_InitNet(SBN_NetInterface_t *NetInterface);
+
+int SBN_DTN_InitPeer(SBN_PeerInterface_t *PeerInterface);
+
+int SBN_DTN_Send(SBN_NetInterface_t *Net, SBN_PeerInterface_t *Peer,
+        SBN_MsgType_t MsgType, SBN_MsgSize_t MsgSize, SBN_Payload_t Payload);
+
+int SBN_DTN_Recv(SBN_NetInterface_t *Net, SBN_MsgType_t *MsgTypePtr,
+        SBN_MsgSize_t *MsgSizePtr, SBN_CpuID_t *CpuIDPtr,
+        SBN_Payload_t PayloadBuffer);
+
+int SBN_DTN_ReportModuleStatus(SBN_ModuleStatusPacket_t *Packet);
+
+int SBN_DTN_ResetPeer(SBN_PeerInterface_t *PeerInterface);
+
+SBN_IfOps_t SBN_DTN_Ops =
+{
+    SBN_DTN_LoadNet,
+    SBN_DTN_LoadPeer,
+    SBN_DTN_InitNet,
+    SBN_DTN_InitPeer,
+    SBN_DTN_Send,
+    NULL,
+    SBN_DTN_Recv,
+    SBN_DTN_ReportModuleStatus,
+    SBN_DTN_ResetPeer
+};
+
+#endif /* _sbn_dtn_if_h_ */
