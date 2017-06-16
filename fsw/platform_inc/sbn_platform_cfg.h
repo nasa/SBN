83287959fde5a31d8190d1ee4b21de76d9833b80
Chris Knight <Christopher.D.Knight@nasa.gov>
2017-06-16 08:02:54 -0700

    cleanup of unused #defines

--- fsw/platform_inc/sbn_platform_cfg.h
+++ fsw/platform_inc/sbn_platform_cfg.h
@@ -51,14 +51,11 @@
 #define SBN_VOL_PEER_FILENAME         "/ram/SbnPeerData.dat"
 #define SBN_NONVOL_PEER_FILENAME      "/cf/SbnPeerData.dat"
 #define SBN_PEER_FILE_LINE_SIZE       128
-#define SBN_NETWORK_MSG_MARGIN        SBN_MAX_NETWORK_PEERS * 2  /* Double it to handle clock skews where a peer */
-                                                             /* can send two messages in my single cycle */
+
 #define SBN_VOL_MODULE_FILENAME       "/ram/SbnModuleData.dat"
 #define SBN_NONVOL_MODULE_FILENAME    "/cf/SbnModuleData.dat"
 #define SBN_MODULE_FILE_LINE_SIZE     128
 #define SBN_MAX_INTERFACE_TYPES       8
-
-#define SBN_SCH_PIPE_DEPTH            10
 
 #define SBN_MOD_STATUS_MSG_SIZE       128 /* bytes */
 
